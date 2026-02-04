83bc8a3513cf098ffd4dce7ee4e829c0fdae3f7f
victoria.bozhko <viktoria.bozhko@jetbrains.com>
2026-02-04 14:51:22 +0100

    Initial commit

---
+++ tests/login.spec.ts
@@ -0,0 +1,8 @@
+import { test, expect } from '@playwright/test';
+
+test('user can submit login form', async ({ page }) => {
+  await page.goto('http://localhost:3000');
+
+  await page.click('#submit-btn');
+  await expect(page.locator('#submit-btn')).toBeVisible();
+});
